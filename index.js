e029bf8c7ff78ab6d737a2270f11fa1161cba2e8
Sara Hopper <sara.hopper@gmail.com>
2017-09-11 16:39:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function getRepositories() {
 function showRepositories(event, data) {
   const repos = JSON.parse(this.responseText)
   const src = document.getElementById("repository-template").innerHTML
-  const template = handlebars.compile(src)
+  const template = Handlebars.compile(src)
   const repoList = template(repos)
   document.getElementById("repositories").innerHTML = repoList
 }
