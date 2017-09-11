8c6d1f47d72c1070338363842246b5bd9d9b5587
Sara Hopper <sara.hopper@gmail.com>
2017-09-11 16:29:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,7 @@ function showRepositories(event, data) {
   const repos = JSON.parse(this.responseText)
   const repoList = '<ul>' + repos.map(r => {
     return (`
-      <li>
-        <h2><a href="${r.html_url}">${r.name}</a></h2>
-        <p>Watchers: ${r.watchers_count}</p>
-        <p>Forks: ${r.forks_count}</p>
-        <p>Issues: ${r.open_issue_count}</p>
-      </li>`
+      `
       )
   }).join('') + "</ul>"
   document.getElementById("repositories").innerHTML = repoList
